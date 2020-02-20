1c7c68e3c6cad1b8df22993924e82896f32ea87c
unknown <olaifawaliu@rocketmail.com>
2020-02-20 16:33:30 +0100

    Addition of Html file

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+# Render helps to display things in our function
 # Create your views here.
+def post_list(request):
+    return render(request, 'blogapp/post_list.html', {})
